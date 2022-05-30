b8f61fa35b28bfb6a5b2563f0658a117d0a0c332
Ethan Huang <ethanferminhuang@gmail.com>
2022-05-29 21:24:18 -0700

    Initial commit

---
+++ src/main/java/bootstrap/controller/SecurityController.java
@@ -0,0 +1,22 @@
+package bootstrap.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController @RequestMapping("welcome")
+public class SecurityController {
+
+    @GetMapping("user")
+    public String helloUser() {
+        return "Hello User";
+    }
+
+    @GetMapping("admin")
+    public String helloAdmin() {
+        return "Hello Admin";
+    }
+
+}
